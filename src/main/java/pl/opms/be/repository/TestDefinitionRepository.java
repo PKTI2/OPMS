6cf23a52310c1bf25b0865438c7c13aaf8b9d03c
Piotr Borczyk <howorang@gmail.com>
2016-12-05 19:53:26 +0100

    Integracja QueryDSL

--- src/main/java/pl/opms/be/repository/TestDefinitionRepository.java
+++ src/main/java/pl/opms/be/repository/TestDefinitionRepository.java
@@ -1,6 +1,8 @@
 package pl.opms.be.repository;
 
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import pl.opms.be.entity.TestDefinitionEntity;
 
@@ -8,5 +10,7 @@ import pl.opms.be.entity.TestDefinitionEntity;
  * Created by Dawid on 22.11.2016 at 18:46.
  */
 @Repository
-public interface TestDefinitionRepository extends CrudRepository<TestDefinitionEntity, Long> {
+public interface TestDefinitionRepository extends PagingAndSortingRepository<TestDefinitionEntity, Long>,
+        QueryDslPredicateExecutor<TestDefinitionEntity> {
+
 }
