add3bcc64bc13bc86e51c5270680d9cdb893578f
Piotr Borczyk <howorang@gmail.com>
2016-12-06 04:29:35 +0100

    Dodano możliwosc wysyłania wiadomości do AbstractController

--- src/main/java/pl/opms/fe/controller/AbstractController.java
+++ src/main/java/pl/opms/fe/controller/AbstractController.java
@@ -1,5 +1,9 @@
 package pl.opms.fe.controller;
 
+import pl.opms.consts.MessageType;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import java.io.Serializable;
 
 /**
@@ -8,4 +12,15 @@ import java.io.Serializable;
 public abstract class AbstractController implements Serializable {
 
     public abstract void init();
+
+    protected void sendMessage(String message, MessageType messageType) {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        context.addMessage(null, new FacesMessage(messageType.getSeverity(),messageType.getMessageTitle(),  message) );
+
+    }
+
+    protected void sendMessage(String message) {
+        sendMessage(message,MessageType.INFO);
+    }
 }
