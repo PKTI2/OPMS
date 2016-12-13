9675ff5a5b73db4e7e3a617c627e315baafe29c2
Piotr Grabowski <ppkgrabowski@gmail.com>
2016-12-13 22:09:02 +0100

    commit techniczny

--- src/main/java/pl/opms/be/entity/UserEntity.java
+++ src/main/java/pl/opms/be/entity/UserEntity.java
@@ -5,29 +5,33 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @EqualsAndHashCode(callSuper = false)
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
-
-@Entity
 @Table(name = "USER_TABLE")
-@Inheritance( strategy = InheritanceType.JOINED )
 public class UserEntity extends BaseEntity {
 
     @Column(name = "username")
-    private String userName;
+    private String username;
 
     @Column(name = "password")
     private String password;
 
-    @Column(name = "personal_data_id")
-    private Long personalDataId;
+    @Column(name = "enabled")
+    private boolean enabled = false;
 
-    @OneToOne (cascade = {CascadeType.ALL})
+    @OneToOne(cascade = CascadeType.MERGE)
+    private RoleEntity roleEntity;
+
+    @OneToOne(cascade = {CascadeType.ALL})
     private PersonalDataEntity personalDataEntity;
-
 
 }
