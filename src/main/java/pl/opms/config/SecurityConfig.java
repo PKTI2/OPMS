8063638358df04e0bbea089a2fd173c2ae62ee89
Dawid Świnoga <dawid.swinoga@gmail.com>
2016-12-30 19:20:55 +0100

    Przeniesiono logowanie użytkonika do UserDetailsServiceImp

--- src/main/java/pl/opms/config/SecurityConfig.java
+++ src/main/java/pl/opms/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package pl.opms.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,24 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/webjars/bootstrap/**").permitAll()
                 .antMatchers("/webjars/jquery/**").permitAll()
                 .antMatchers("/css/**", "/login").permitAll()
+                .antMatchers("/css/**", "/resetPassword").permitAll()
                 .antMatchers("/**").hasRole("USER")
                 .and()
                 .formLogin()
                 .loginPage("/login");
-    }
-
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(passwordEncoder())
-                .usersByUsernameQuery("SELECT username, password, enabled FROM user_table WHERE username=?")
-                .authoritiesByUsernameQuery("SELECT username, name_role FROM user_table\n" +
-                        "JOIN role_privilege_entities\n" +
-                        "ON role_privilege_entities.role_entity_id = USER_TABLE.role_entity_id\n" +
-                        "JOIN privilege_entity\n" +
-                        "ON role_privilege_entities.privilege_entities_id = privilege_entity.id\n" +
-                        "WHERE username =?");
     }
 
     @Bean
