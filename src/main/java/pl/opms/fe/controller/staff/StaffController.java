d4c0fbcfe92a951a8e49c29df1019692c9940162
Dawid Świnoga <dawid.swinoga@gmail.com>
2016-12-31 13:15:27 +0100

    dodano

---
+++ src/main/java/pl/opms/fe/controller/staff/StaffController.java
@@ -0,0 +1,22 @@
+package pl.opms.fe.controller.staff;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Dawid on 30.12.2016 at 22:31.
+ */
+@Controller
+@RequestMapping("/staff")
+public class StaffController {
+
+    @RequestMapping(path = "/profile")
+    public String showProfile(@RequestParam(required = false, name = "message") String message, ModelMap modelMap) {
+        if (message != null) {
+            modelMap.addAttribute("message", message);
+        }
+        return "staff/profile";
+    }
+}
