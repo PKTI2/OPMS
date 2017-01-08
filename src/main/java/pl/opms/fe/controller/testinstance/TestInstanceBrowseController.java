7da0a25cddce882dd154f0b34b700b5251be87a8
Dawid Świnoga <dawid.swinoga@gmail.com>
2017-01-08 15:33:34 +0100

    dodano wyświeltanie wykresów

--- src/main/java/pl/opms/fe/controller/testinstance/TestInstanceBrowseController.java
+++ src/main/java/pl/opms/fe/controller/testinstance/TestInstanceBrowseController.java
@@ -1,13 +1,10 @@
 package pl.opms.fe.controller.testinstance;
 
-import com.querydsl.core.types.Predicate;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,15 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.opms.be.entity.PatientEntity;
-import pl.opms.be.entity.QTestDefinitionEntity;
 import pl.opms.be.entity.QTestInstanceEntity;
-import pl.opms.be.entity.TestDefinitionEntity;
-import pl.opms.be.entity.TestInstanceEntity;
 import pl.opms.be.service.PatientService;
-import pl.opms.be.service.TestInstanceService;
+import pl.opms.be.utils.chart.ChartData;
+import pl.opms.be.utils.chart.ChartDataPicker;
 import pl.opms.fe.controller.testdefinition.TestDefinitionCrudController;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by howor on 07.01.2017.
@@ -55,9 +51,18 @@ public class TestInstanceBrowseController {
     public ModelAndView crud(@RequestParam(required = true) Long patientId) {
         ModelAndView modelView = new ModelAndView("/test-instance/browse");
         patientEntity = patientService.findOne(patientId);
-        modelView.addObject("patientEntity",patientEntity);
-        modelView.addObject("testInstances",patientEntity.getTests());
+        modelView.addObject("patientEntity", patientEntity);
+        modelView.addObject("testInstances", patientEntity.getTests());
         return modelView;
+    }
+
+    @RequestMapping(path = "/open", params = "testIdForChart", method = RequestMethod.POST)
+    public String showChart(@RequestParam(value = "testIdForChart") Integer id, ModelMap modelMap) {
+        Long testDefId = patientEntity.getTests().get(id).getTestDefinitionEntity().getId();
+        ChartDataPicker chartDataPicker = new ChartDataPicker(patientEntity, testDefId);
+        List<ChartData> chartDataList = chartDataPicker.getChartData();
+        modelMap.addAttribute("list", chartDataList);
+        return "/test-instance/chart";
     }
 
     @RequestMapping(path = "/search", method = RequestMethod.POST)
@@ -69,7 +74,7 @@ public class TestInstanceBrowseController {
     @RequestMapping(path = "/open", method = RequestMethod.POST)
     public String showDefinition(@RequestParam(required = true) Integer rowIndex,
                                  RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("testInstance",patientEntity.getTests().get(rowIndex));
+        redirectAttributes.addFlashAttribute("testInstance", patientEntity.getTests().get(rowIndex));
         return "redirect:/test-instance/display";
     }
 
